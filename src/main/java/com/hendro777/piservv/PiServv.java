b56cbe5fe8a1cf2129150a354aa99ad2e13da042
Hendrik <Hendrik-Lind@web.de>
2023-05-08 23:37:08 +0200

    standard java

---
+++ src/main/java/com/hendro777/piservv/PiServv.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.hendro777.piservv;
+
+/**
+ *
+ * @author hendrik
+ */
+public class PiServv {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
